90a2ce51e7bc324c60d7fbc421b06b21389bacac
Adnan Zameer <adnan.zameer@outlook.com>
2020-07-28 18:18:27 +0100

    Add project files.

---
+++ JSONSplitter/Main.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace JSONSplitter
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.wbMessage = new System.Windows.Forms.WebBrowser();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.fbd = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(12, 44);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(201, 79);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Select Path";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.Enabled = false;
+            this.btnSplit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSplit.Location = new System.Drawing.Point(301, 154);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(249, 79);
+            this.btnSplit.TabIndex = 2;
+            this.btnSplit.Text = "Split Files";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // wbMessage
+            // 
+            this.wbMessage.Location = new System.Drawing.Point(12, 254);
+            this.wbMessage.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbMessage.Name = "wbMessage";
+            this.wbMessage.Size = new System.Drawing.Size(874, 755);
+            this.wbMessage.TabIndex = 5;
+            // 
+            // lblPath
+            // 
+            this.lblPath.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblPath.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPath.Location = new System.Drawing.Point(233, 44);
+            this.lblPath.Margin = new System.Windows.Forms.Padding(3);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(640, 79);
+            this.lblPath.TabIndex = 6;
+            this.lblPath.Text = "No path selected";
+            this.lblPath.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // fbd
+            // 
+            this.fbd.ShowNewFolderButton = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(898, 1023);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.wbMessage);
+            this.Controls.Add(this.btnSplit);
+            this.Controls.Add(this.btnBrowse);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.Text = "Json Array Splitter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnSplit;
+        private System.Windows.Forms.WebBrowser wbMessage;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.FolderBrowserDialog fbd;
+    }
+}
+
---
+++ JSONSplitter/Main.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace JSONSplitter
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            fbd.RootFolder = Environment.SpecialFolder.MyComputer;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                lblPath.Text = fbd.SelectedPath;
+                btnSplit.Enabled = true;
+            }
+
+            wbMessage.DocumentText = "";
+        }
+
+        private void btnSplit_Click(object sender, EventArgs e)
+        {
+            wbMessage.DocumentText = "";
+            var path = lblPath.Text;
+
+            if (!string.IsNullOrEmpty(path) && !path.Equals("No path selected"))
+            {
+                string[] fileEntries = Directory.GetFiles(path);
+
+                var totalJsonFiles = fileEntries.Where(x => x.EndsWith(".json"));
+                if (totalJsonFiles.Any())
+                {
+
+                    var confirmationMesssage =
+                        "Split action will be perform on the following " + totalJsonFiles.Count() + " JSON files." + Environment.NewLine + Environment.NewLine;
+                    foreach (var filePath in fileEntries)
+                    {
+                        if (filePath.EndsWith(".json"))
+                        {
+                            var file = filePath.Replace(path, "").Replace(".json", "").Replace("\\", "")
+                                .Replace("/", "");
+
+                            confirmationMesssage = confirmationMesssage + "- " + file + ".json" + Environment.NewLine;
+                        }
+                    }
+
+                    MessageBox.Show(confirmationMesssage);
+                    var message = "";
+                    foreach (var filePath in fileEntries)
+                    {
+                        if (filePath.EndsWith(".json"))
+                        {
+                            var file = filePath.Replace(path, "").Replace(".json", "").Replace("\\", "")
+                                .Replace("/", "");
+
+                            message = message + SplitJsonFile(file, path);
+                        }
+                    }
+
+                    wbMessage.DocumentText = message;
+
+                }
+                else
+                {
+                    MessageBox.Show("No JSON file(s) in the path " + path);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Select path to JSON file(s)");
+            }
+        }
+
+        private string SplitJsonFile(string file, string path)
+        {
+            try
+            {
+                using (StreamReader r = new StreamReader(path + "\\" + file + ".json"))
+                {
+                    var folderName = path + "\\" + file;
+
+                    if (Directory.Exists(folderName))
+                    {
+                        var dir = new DirectoryInfo(folderName);
+                        foreach (var f in dir.GetFiles())
+                        {
+                            f.Delete();
+                        }
+                    }
+                    else
+                        Directory.CreateDirectory(folderName);
+
+                    var json = r.ReadToEnd();
+
+                    dynamic stuff = JsonConvert.DeserializeObject(json);
+                    var count = 1;
+                    foreach (var s in stuff)
+                    {
+                        string fileName = file + count + ".json";
+                        var filePath = System.IO.Path.Combine(folderName, fileName);
+                        WriteToJsonFile(filePath, s, false);
+                        count++;
+
+                    }
+
+                    return "<strong>" + (count - 1) +
+                              "</strong> JSON files have been split from <strong>" + file +
+                              ".json</strong> file in the path: <strong>" + folderName + "</strong> </br> </br>";
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return "";
+        }
+
+        private void WriteToJsonFile(string filePath, dynamic objectToWrite, bool append = false)
+        {
+            TextWriter writer = null;
+            try
+            {
+                writer = new StreamWriter(filePath, append);
+                writer.Write(objectToWrite);
+            }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
+            }
+        }
+    }
+}
